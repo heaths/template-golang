5b9d7c8cda9d298a3fe80565ecad6cbdb4c0ac09
Heath Stewart <heaths@outlook.com>
2022-11-24 17:04:19 -0800

    Use v0.2.0 of heaths/gh-template extension

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-// Copyright {{param "copyright" "2022" "What is the copyright year?"}} Heath Stewart.
+// Copyright {{param "copyright"}} Heath Stewart.
 // Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 
 package main
